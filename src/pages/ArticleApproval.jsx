1c18be5f55a3c57da3729dd81722659caf9703a5
SardorQobilov <sardorqobilov555@gmail.com>
2024-04-17 23:31:01 +0500

    project fininshed

--- src/pages/ArticleApproval.jsx
+++ src/pages/ArticleApproval.jsx
@@ -8,84 +8,59 @@ const ArticleApproval = () => {
         bannerBg={true}
         title="Высокий уровень вовлечения представителей."
       />
-      <div className="lg:max-w-[760px] max-w-[95%] mx-auto space-y-6 text-[#696F79] text-lg leading-[31px] font-light">
-        <p>
-          <div className="flex items-start lg:mb-[-23px] gap-4">
-            <span className="text-teal text-[64px] block leading-normal">
-              Р
-            </span>
-            <p className="mt-[10px]">
-              {" "}
-              равным образом, постоянный количественный рост и сфера нашей{" "}
-              активности способствует повышению качества первоочередных
-              требований.{" "}
-            </p>
-          </div>
-          В частности, высокотехнологичная концепция общественного уклада в
-          значительной степени обусловливает важность дальнейших направлений
-          развития. Учитывая ключевые сценарии поведения, высококачественный
-          прототип будущего проекта способствует повышению качества
-          экономической целесообразности принимаемых решений. Следует отметить,
-          что новая модель организационной деятельности напрямую зависит от
-          переосмысления внешнеэкономических политик.
-        </p>
-        <p>
-          Значимость этих проблем настолько очевидна, что сплочённость команды
-          профессионалов не оставляет шанса для системы массового участия.
-          Имеется спорная точка зрения, гласящая примерно следующее: базовые
-          сценарии поведения пользователей неоднозначны и будут описаны
-          максимально подробно. Высокий уровень вовлечения представителей
-          целевой аудитории является четким доказательством простого факта:
-          социально-экономическое развитие способствует подготовке и реализации
-          стандартных подходов. Повседневная практика показывает, что повышение
-          уровня гражданского сознания требует от нас анализа благоприятных
-          перспектив.
-        </p>
-        <p>
-          С учётом сложившейся международной обстановки, убеждённость некоторых
-          оппонентов не даёт нам иного выбора, кроме определения направлений
-          прогрессивного развития.
-        </p>
-      </div>
       <div className="container">
-        <div className="tendence__bg">
-          <h3 className="text-teal text-[32px] font-semibold">
-            Вот вам яркий пример современных тенденций —{" "}
-          </h3>
-        </div>
-      </div>
-      <div className="lg:max-w-[760px] max-w-[95%] mx-auto space-y-6 text-[#696F79] text-lg leading-[31px] font-light">
-        <p>
-          сложившаяся структура организации не даёт нам иного выбора, кроме
-          определения своевременного выполнения сверхзадачи. Но реплицированные
-          с зарубежных источников, современные исследования призваны к ответу.
-        </p>
-        <p>
-          Равным образом, постоянный количественный рост и сфера нашей
-          активности способствует повышению качества первоочередных требований.
-          В частности, высокотехнологичная концепция общественного уклада в
-          значительной степени обусловливает важность дальнейших направлений
-          развития. Учитывая ключевые сценарии поведения, высококачественный
-          прототип будущего экономической целесообразности принимаемых решений.
-          Следует отметить, что новая модель организационной деятельности
-          напрямую зависит от переосмысления внешнеэкономических политик.
-        </p>
-        <p>
-          экономической целесообразности принимаемых решений. Следует отметить,
-          что новая модель организационной деятельности напрямую зависит от
-          переосмысления внешнеэкономических политик.
-        </p>
-      </div>
-      <div className="lg:max-w-[760px] max-w-[95%] mx-auto mt-[60px]">
-        <div className="md:p-[50px] p-4 border-t-[4px] border-teal bg-[#EAFFFE] rounded-lg">
-          <p className="sm:text-2xl text-base">
+        <div className="space-y-[43px] max-w-[760px] mx-auto mt-[62px]">
+          <p className="text-[#696F79] leading-[30.6px] text-lg font-light">
+            Равным образом, постоянный количественный рост и сфера нашей
+            активности способствует повышению качества первоочередных
+            требований. В частности, высокотехнологичная концепция общественного
+            уклада в значительной степени обусловливает важность дальнейших
+            направлений развития. Учитывая ключевые сценарии поведения,
+            высококачественный прототип будущего проекта способствует повышению
+            качества экономической целесообразности принимаемых решений. Следует
+            отметить, что новая модель организационной деятельности напрямую
+            зависит от переосмысления внешнеэкономических политик.
+          </p>
+          <p className="text-[#696F79] leading-[30.6px] text-lg font-light">
+            Значимость этих проблем настолько очевидна, что сплочённость команды
+            профессионалов не оставляет шанса для системы массового участия.
+            Имеется спорная точка зрения, гласящая примерно следующее: базовые
+            сценарии поведения пользователей неоднозначны и будут описаны
+            максимально подробно. Высокий уровень вовлечения представителей
+            целевой аудитории является четким доказательством простого факта:
+            социально-экономическое развитие способствует подготовке и
+            реализации стандартных подходов. Повседневная практика показывает,
+            что повышение уровня гражданского сознания требует от нас анализа
+            благоприятных перспектив.
+          </p>
+          <p className="text-[#696F79] leading-[30.6px] text-lg font-light">
+            С учётом сложившейся международной обстановки, убеждённость
+            некоторых оппонентов не даёт нам иного выбора, кроме определения
+            направлений прогрессивного развития.
+          </p>
+          <p className="text-[#696F79] leading-[30.6px] text-lg font-light">
+            Вот вам яркий пример современных тенденций - сложившаяся структура
+            организации не даёт нам иного выбора, кроме определения
+            своевременного выполнения сверхзадачи. Но реплицированные с
+            зарубежных источников, современные исследования призваны к ответу.
+          </p>
+          <p className="text-[#696F79] leading-[30.6px] text-lg font-light">
+            Равным образом, постоянный количественный рост и сфера нашей
+            активности способствует повышению качества первоочередных
+            требований. В частности, высокотехнологичная концепция общественного
+            уклада в значительной степени обусловливает важность дальнейших
+            направлений развития. Учитывая ключевые сценарии поведения,
+            высококачественный прототип будущего экономической целесообразности
+            принимаемых решений. Следует отметить, что новая модель
+            организационной деятельности напрямую зависит от переосмысления
+            внешнеэкономических политик.
+          </p>
+          <p className="text-[#696F79] leading-[30.6px] text-lg font-light">
             Господа, перспективное планирование создаёт предпосылки для
             инновационных методов управления процессами. Высокий уровень
             вовлечения представителей целевой аудитории является.
           </p>
-        </div>
-        <div className="mt-16 mb-[95px]">
-          <p className="text-[#696F79] text-lg leading-[31px] font-light">
+          <p className="text-[#696F79] leading-[30.6px] text-lg font-light">
             Равным образом, постоянный количественный рост и сфера нашей
             активности способствует повышению качества первоочередных
             требований. В частности, высокотехнологичная концепция общественного
@@ -97,69 +72,84 @@ const ArticleApproval = () => {
             зависит от переосмысления внешнеэкономических политик.
           </p>
         </div>
-        <div className="flex gap-[10px]">
-          <button className="category__btn">Категория 1</button>
-          <button className="category__btn">Категория 2</button>
-          <button className="category__btn">Категория 3</button>
+        <div className="flex gap-[10px] max-w-[800px] mx-auto mt-[14px] mb-8">
+          <button
+            className="py-[5px] px-[10px] border border-[#E7E7E7] text-[#696F79] text-xs leading-6 font-light"
+            aria-label="Категория 1"
+          >
+            Категория 1
+          </button>
+          <button
+            className="py-[5px] px-[10px] border border-[#E7E7E7] text-[#696F79] text-xs leading-6 font-light"
+            aria-label="Категория 1"
+          >
+            Категория 2
+          </button>
+          <button
+            className="py-[5px] px-[10px] border border-[#E7E7E7] text-[#696F79] text-xs leading-6 font-light"
+            aria-label="Категория 1"
+          >
+            Категория 3
+          </button>
         </div>
-      </div>
-      <div className="container my-8 h-[0.5px] bg-[#C4C4C4]"></div>
-      <div className="lg:max-w-[760px] max-w-[95%] mx-auto mb-[108px]">
-        <div className="flex flex-wrap gap-y-4 items-center justify-between pb-8">
-          <div className="flex items-center gap-[30px]">
-            <img
-              src={assets.userAvatar}
-              alt="user img"
-              width="80"
-              height="80"
-            />
-            <div className="space-y-[2px] text-[#242628]">
-              <h6 className="font-semibold text-base leading-[21px]">
-                Иванов Иван
-              </h6>
-              <span className="inline-block text-xs leading-[19px] font-light">
-                Врач
-              </span>
+        <div className="w-full h-[0.5px] bg-[#C4C4C4]"></div>
+        <div className="max-w-[800px] mx-auto mt-8">
+          <div className="mb-[66px] flex-wrap gap-4 flex items-center justify-between">
+            <div className="flex items-center gap-[30px]">
+              <img
+                src={assets.userAvatar}
+                alt="user img"
+                width="80"
+                height="80"
+              />
+              <div className="space-y-[2px] text-[#242628]">
+                <h6 className="font-semibold text-base leading-[21px]">
+                  Иванов Иван
+                </h6>
+                <span className="inline-block text-xs leading-[19px] font-light">
+                  Врач
+                </span>
+              </div>
+            </div>
+            <div className="flex gap-[15px]">
+              <a
+                href="#!"
+                aria-label="social media wk"
+                className="rounded-full bg-[#495057] w-10 h-10 flex justify-center items-center"
+              >
+                <img src={assets.wk} alt="wk icon" width="22" height="14" />
+              </a>
+              <a
+                href="#!"
+                aria-label="social media telegram"
+                className="rounded-full bg-[#495057] w-10 h-10 flex justify-center items-center"
+              >
+                <img
+                  src={assets.telegram}
+                  alt="telegram icon"
+                  width="19"
+                  height="17"
+                />
+              </a>
+              <a
+                href="#!"
+                aria-label="social media whatsapp"
+                className="rounded-full bg-[#495057] w-10 h-10 flex justify-center items-center"
+              >
+                <img
+                  src={assets.whatsapp}
+                  alt="whatsapp icon"
+                  width="21"
+                  height="21"
+                />
+              </a>
             </div>
           </div>
-          <div className="flex gap-[15px]">
-            <a
-              href="#!"
-              aria-label="social media wk"
-              className="rounded-full bg-[#495057] w-10 h-10 flex justify-center items-center"
-            >
-              <img src={assets.wk} alt="wk icon" width="22" height="14" />
-            </a>
-            <a
-              href="#!"
-              aria-label="social media telegram"
-              className="rounded-full bg-[#495057] w-10 h-10 flex justify-center items-center"
-            >
-              <img
-                src={assets.telegram}
-                alt="telegram icon"
-                width="19"
-                height="17"
-              />
-            </a>
-            <a
-              href="#!"
-              aria-label="social media whatsapp"
-              className="rounded-full bg-[#495057] w-10 h-10 flex justify-center items-center"
-            >
-              <img
-                src={assets.whatsapp}
-                alt="whatsapp icon"
-                width="21"
-                height="21"
-              />
-            </a>
-          </div>
+          <Comments />
         </div>
-        <Comments />
-      </div>
-      <div className="container mt-[58px] mb-[124px]">
-        <News length={3} showTitle={true} title="Вам может понравиться" />
+        <div className="container mt-[58px] mb-[124px]">
+          <News length={3} showTitle={true} title="Вам может понравиться" />
+        </div>
       </div>
       <div className="container flex justify-between flex-wrap gap-4 mb-[88px]">
         <button className="border-[6px] border-[#FF0000] bg-[#C4C4C4] text-black max-w-[453px] h-[72px] w-full text-2xl font-semibold">
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -14,8 +14,8 @@ const Chat = () => {
       <Header color={true} darkColor={true} />
 
       <div className="flex items-stretch mt-[90px] h-[1060px]">
-        <div className="md:max-w-[414px] max-w-[300px] hidden sm:block  bg-teal py-3 rounded-lg relative">
-          <div className="relative flex items-center gap-[26px] px-6 justify-between pb-[23px] border-b border-black">
+        <div className="md:max-w-[414px] sm:max-w-[300px] w-[150px] block  bg-teal py-3 rounded-lg relative">
+          <div className="relative flex sm:flex-row flex-col items-center gap-[26px] sm:px-6 px-4 justify-between pb-[23px] border-b border-black">
             <div className="relative max-w-[300px] border border-black h-10 bg-[#E7E7E7B8] rounded-[10px] overflow-hidden ">
               <input
                 className="w-full placeholder:text-white text-xl text-white bg-transparent h-full pl-[43px]"
@@ -23,7 +23,7 @@ const Chat = () => {
                 placeholder="Поиск"
               />
               <img
-                className="absolute top-1/2 -translate-y-1/2 left-[18px]"
+                className="absolute top-1/2 -translate-y-1/2 sm:left-[18px] left-[10px]"
                 src={assets.search}
                 alt=""
                 width={17}
@@ -34,7 +34,7 @@ const Chat = () => {
               <img src={assets.plus} alt="" />
             </button>
             <div
-              className={`absolute top-full left-0 w-full h-[207px] duration-300 bg-white p-4 origin-top scale-y-0 ${
+              className={`absolute top-full sm:right-0 left-0 w-fit h-[207px] duration-300 bg-white p-4 origin-top scale-y-0 ${
                 open ? "scale-y-100" : ""
               }`}
             >
@@ -43,7 +43,7 @@ const Chat = () => {
               </p>
             </div>
           </div>
-          <div className="flex items-center gap-[9px] px-6 py-8 border-b border-black">
+          <div className="flex items-center gap-[9px] sm:px-6 px-4 py-8 border-b border-black">
             <img
               className="rounded-full"
               src={assets.doctorImg}
@@ -51,7 +51,9 @@ const Chat = () => {
               width={56}
               height={53}
             />
-            <h4 className="text-2xl text-white font-bold">Иванова Татьяна</h4>
+            <h4 className="sm:text-2xl text-sm text-white font-bold sm:block hidden">
+              Иванова Татьяна
+            </h4>
             <div className="w-[17px] h-[17px] bg-[#97FF46] rounded-full"></div>
           </div>
         </div>
@@ -65,8 +67,10 @@ const Chat = () => {
                 width={56}
                 height={53}
               />
-              <h4 className="text-2xl text-black font-bold">Иванова Татьяна</h4>
-              <div className="w-[17px] h-[17px] bg-[#97FF46] rounded-full"></div>
+              <h4 className="sm:text-2xl text-base text-black font-bold">
+                Иванова Татьяна
+              </h4>
+              <div className="sm:w-[17px] w-[10px] h-[10px] sm:max-h-[17px] bg-[#97FF46] rounded-full"></div>
             </div>
             <div className="pt-[69px] space-y-10 px-[33px]">
               <div className="flex items-center w-full gap-4 justify-end">
@@ -91,13 +95,13 @@ const Chat = () => {
               </div>
             </div>
           </div>
-          <div className="py-8 px-[50px] border-t border-[#B2B2B2]">
+          <div className="py-8 sm:px-[50px] px-4 border-t border-[#B2B2B2]">
             <label
               className="flex items-center cursor-pointer gap-[20px]"
               htmlFor="file"
             >
               <img src={assets.file} alt="file" />
-              <span className="sm:text-2xl text-base text-[#B2B2B2]">
+              <span className="sm:text-2xl text-xs text-[#B2B2B2]">
                 Напишите здесь своё сообщение
               </span>
             </label>
